5547145662d5405688be2cfe9660cc209a6e5c92
Jon Borgonia <jon@gomagames.com>
2015-04-16 19:36:05 -1000

    game.js is clean, ready for implementation

--- js/game.js
+++ js/game.js
@@ -1,128 +1,71 @@
 GAME = {};
+
+/**
+ * Runs once, at the beginning of the application
+ * 
+ * @param  {Object} JumpSlide The Game Engine
+ */
 GAME.init = function (JumpSlide) {
 
-  var platform_height = 45;
-  var platform_width = 200;
-  var floor_height = 68;
-  var floor_position = 700;
-  var coin = 50;
-
-
-  JumpSlide.addPlatform(50, floor_position, 300, floor_height);
-  JumpSlide.addPlatform(480, floor_position, 1000, floor_height);
-
-  /* Upper Route */
-  // JumpSlide.addPlatform(825, 517, coin, coin);
-  JumpSlide.addPlatform(750, 580, platform_width, platform_height);
-  // JumpSlide.addPlatform(825, 637, coin, coin);
-
-  // JumpSlide.addPlatform(1110, 370, coin, coin);
-  JumpSlide.addPlatform(1060, 480, platform_width, platform_height);
-
-  // JumpSlide.addPlatform(1390, 270, coin, coin);
-  JumpSlide.addPlatform(1350, 380, platform_width, platform_height);
-
-  // JumpSlide.addPlatform(1690, 170, coin, coin);
-  JumpSlide.addPlatform(1640, 280, platform_width, platform_height);
-
-  // JumpSlide.addPlatform(1990, 70, coin, coin);
-  JumpSlide.addPlatform(1920, 180, platform_width, platform_height);
-
-  JumpSlide.addCoin(2080, 70);
-  JumpSlide.addCoin(2140, 100);
-  JumpSlide.addCoin(2250, 180);
-  JumpSlide.addCoin(2280, 250);
-  JumpSlide.addCoin(2320, 375);
-  JumpSlide.addCoin(2345, 600);
   
-
-  /* Lower Route */
-  JumpSlide.addPlatform(1610, floor_position, 1000, floor_height);
-  JumpSlide.addPlatform(2740, floor_position, 1000, floor_height);
-
-  // win condition
-  JumpSlide.addGoal(2845, 650);
-
-  JumpSlide.start();
-
 }
 
+/**
+ * Runs every animation game loop
+ *   60 times per second
+ *   Only write code in here that should be run many times!
+ *   
+ * @param  {Object} JumpSlide The Game Engine
+ */
 GAME.loop = function (JumpSlide) {
 
-  // make the player run right
-  // by looping through each platform
-  JumpSlide.forEachPlatform(function(platform) {
-    // translate it's x position
-    platform.position.x -= JumpSlide.SETTINGS.run_speed;
-  });
-
-  // loop through each coin
-  JumpSlide.forEachCoin(function(coin) {
-
-    // translate it's x position
-    coin.position.x -= JumpSlide.SETTINGS.run_speed;
-
-    // check if player is touching coin
-    if(JumpSlide.player.check_collision(coin)){
-
-      // collect the coin to score
-      JumpSlide.collectCoin(coin);
-
-      // play coin sound effects
-      JumpSlide.sfx.coin.play();
-    }
-  });
-
-  // loop through each platform
-  JumpSlide.forEachGoal(function(goal) {
-    // translate it's x position
-    goal.position.x -= JumpSlide.SETTINGS.run_speed;
-
-    // victory condition
-    if(JumpSlide.player.check_collision(goal)){
-      JumpSlide.game_win();
-    }
-  });
-
-  // check if player falls, then lose game
-  if( JumpSlide.player.position.y >= JumpSlide.SETTINGS.ipad_dimensions.height ){
-    JumpSlide.game_lose();
-  }
 
 }
 
+/**
+ * Runs once, when the player taps or clicks the stage
+ * 
+ * @param  {Point} point  the position of the touch event
+ */
 GAME.tap = function ( point ) {
 
-  // check if player touches top part of screen
-  if( point.y < JumpSlide.SETTINGS.controls.up ){
-
-    // make player jump
-    JumpSlide.player.jump();
-  }
   
 }
 
+/**
+ * Runs once, when the player holds the tap or mouse button down
+ * 
+ * @param  {Point} point  the position of the touch event
+ */
 GAME.touch_start = function ( point ) {
 
-  // check if player touches bottom part of screen
-  if( point.y > JumpSlide.SETTINGS.controls.down ){
-
-    // make player start sliding
-    JumpSlide.player.slide();
-  }
 
 }
 
+/**
+ * Runs once, when the player releases the tap or mouse button
+ * 
+ * @param  {Point} point  the position of the touch event
+ */
 GAME.touch_end = function ( point ) {
 
-  JumpSlide.player.stop_sliding();
 
 }
 
+/**
+ * Runs once, when the player wins the game
+ * 
+ * @param  {Object} JumpSlide The Game Engine
+ */
 GAME.win = function (JumpSlide) {
   
 }
 
+/**
+ * Runs once, when the player loses the game
+ * 
+ * @param  {Object} JumpSlide The Game Engine
+ */
 GAME.lose = function (JumpSlide) {
   
 }
